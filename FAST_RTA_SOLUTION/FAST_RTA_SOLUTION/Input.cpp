aa5b0c32d19c81af24893953b48a09fb57c4e5d8
josh ronek <jronek@gmail.com>
2016-10-29 23:12:16 -0400

    input nonsense

---
+++ FAST_RTA_SOLUTION/FAST_RTA_SOLUTION/Input.cpp
@@ -0,0 +1,33 @@
+#include "Input.h"
+
+
+
+Input::Input()
+{
+}
+
+Input::Input(const Input &)
+{
+}
+
+
+Input::~Input()
+{
+}
+
+void Input::Initialize()
+{
+}
+
+void Input::KeyDown(unsigned int)
+{
+}
+
+void Input::KeyUp(unsigned int)
+{
+}
+
+bool Input::IsKeyDown(unsigned int)
+{
+	return false;
+}

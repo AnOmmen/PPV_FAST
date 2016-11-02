c9388bfed691ed3c08b996e4d0f3ef131a60dcc0
MrLuckee <collin.p.ommen@gmail.com>
2016-11-02 18:45:51 -0400

    AnimationSet Progress

--- FAST_RTA_SOLUTION/FAST_RTA_SOLUTION/AnimationSet.cpp
+++ FAST_RTA_SOLUTION/FAST_RTA_SOLUTION/AnimationSet.cpp
@@ -2,7 +2,7 @@
 
 
 
-AnimationSet::AnimationSet()
+AnimationSet::AnimationSet() : m_bindPose(nullptr), m_defaultAnim(NULL)
 {
 }
 
@@ -18,17 +18,34 @@ bool AnimationSet::AddAnimation(FILE * _file)
 
 bool AnimationSet::AddBindPose(const BindPose * _pose)
 {
+	if (nullptr != _pose)
+	{
+		m_bindPose = _pose;
+		return true;
+	}
 	return false;
 }
 
-const Animation * AnimationSet::GetAnimation(int _key)
+unsigned int AnimationSet::GetAnimationCount() const
 {
+	return m_animations.size();
+}
+
+const Animation * AnimationSet::GetAnimation(int _key) const
+{
+	if (_key < m_animations.size())
+		return &m_animations[_key];
 	return nullptr;
 }
 
 const BlendInfo * AnimationSet::GetBlendInfo(int _animationFrom, int _animationTo)
 {
 	return nullptr;
+}
+
+const BindPose * AnimationSet::GetBindPose() const
+{
+	return m_bindPose;
 }
 
 const Animation * AnimationSet::GetDefaultAnimation()
--- FAST_RTA_SOLUTION/FAST_RTA_SOLUTION/AnimationSet.h
+++ FAST_RTA_SOLUTION/FAST_RTA_SOLUTION/AnimationSet.h
@@ -5,17 +5,19 @@
 
 class AnimationSet
 {
-public:
 	std::vector<Animation> m_animations;
 	const BindPose* m_bindPose;
 	std::vector<std::vector<BlendInfo> > m_blendTimes;
 	int m_defaultAnim;
+public:
 	AnimationSet();
 	~AnimationSet();
 	bool AddAnimation(FILE* _file);
 	bool AddBindPose(const BindPose* _pose);
-	const Animation* GetAnimation(int _key);
+	unsigned int GetAnimationCount() const;
+	const Animation* GetAnimation(int _key) const;
 	const BlendInfo* GetBlendInfo(int _animationFrom, int _animationTo);
+	const BindPose* GetBindPose() const;
 	const Animation* GetDefaultAnimation();
 };
 
